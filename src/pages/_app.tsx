8b090ab9495a3abbd165e4e2ae9d69092a6bef8e
Ben Fox <ben@lattice.com>
2022-06-20 13:55:22 -0700

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { ChakraProvider } from '@chakra-ui/react';
 
 import '@fontsource/open-sans';
@@ -9,30 +11,51 @@ import '@fontsource/poppins/800.css';
 import { DefaultSeo } from 'next-seo';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import theme from '../theme';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ChakraProvider resetCSS theme={theme}>
-    <Head>
-      <link rel="shortcut icon" href="/images/logo.png" />
-    </Head>
-    <DefaultSeo
-      titleTemplate="%s | Ben Fox's Blog"
-      openGraph={{
-        type: 'website',
-        locale: 'en_US',
-        url: 'https://www.nullfox.com/',
-        site_name: "Ben Fox's Blog",
-      }}
-      twitter={{
-        handle: '@thenullfox',
-        site: '@thenullfox',
-        cardType: 'summary',
-      }}
-    />
-    <Component {...pageProps} />
-  </ChakraProvider>
-);
+type AppWindow = typeof window & {
+  gtag: (t: string, id: string, opts?: Record<string, any>) => void;
+};
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      (window as AppWindow).gtag('config', 'G-06MZCKWRQ9', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/images/logo.png" />
+      </Head>
+      <DefaultSeo
+        titleTemplate="%s | Ben Fox's Blog"
+        openGraph={{
+          type: 'website',
+          locale: 'en_US',
+          url: 'https://www.nullfox.com/',
+          site_name: "Ben Fox's Blog",
+        }}
+        twitter={{
+          handle: '@thenullfox',
+          site: '@thenullfox',
+          cardType: 'summary',
+        }}
+      />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
 
 export default App;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,24 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-06MZCKWRQ9"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
